80d3b902d911e7cf132ee9eee735b1de39382c3e
charri <thomascharriere@gmail.com>
2014-10-31 14:01:35 +0100

    add: view model. setup for dependency injection

---
+++ src/src/ch/hsr/nsg/themenrundgang/view/DetailActivity.java
@@ -0,0 +1,14 @@
+package ch.hsr.nsg.themenrundgang.view;
+
+import ch.hsr.nsg.themenrundgang.vm.DetailViewModel;
+import android.app.Activity;
+
+public class DetailActivity extends Activity {
+
+	private DetailViewModel viewModel;
+	
+	public DetailActivity()
+	{
+		
+	}
+}
---
+++ src/src/ch/hsr/nsg/themenrundgang/vm/AbstractViewModel.java
@@ -0,0 +1,5 @@
+package ch.hsr.nsg.themenrundgang.vm;
+
+public class AbstractViewModel {
+
+}
